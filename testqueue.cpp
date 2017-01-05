8ccaf8c41ad8ce8011636d866dab1fb407a0b1ba
flysnowtan <xuefeitan@gmail.com>
2017-01-05 21:18:30 +0800

    Create testqueue.cpp

---
+++ testqueue.cpp
@@ -0,0 +1,59 @@
+#include "tqueue.h"
+#include <iostream>
+#include<sys/time.h>
+
+using namespace std;
+
+#define MAX_ARRAY_SIZE 10000000
+
+void *PushThread(void* arg)
+{
+    TRingQueue * queue = (TRingQueue*) arg;
+
+    for(int i = 0; i < MAX_ARRAY_SIZE; i++)
+    {   
+        if(!queue->Push(i))
+        {   
+            i--;
+            continue;
+        }   
+    }   
+}
+
+void *PopThread(void* arg)
+{
+    TRingQueue * queue = (TRingQueue*) arg;
+
+    int data;
+    while(true)
+    {   
+        if(queue->Pop(&data, false))
+        {   
+            sched_yield();
+        }   
+        else 
+        {   
+            return NULL;
+        }   
+    }   
+}
+
+int main(void)
+{
+    TRingQueue inQueue;
+    cout <<"start!"<<endl;
+
+    struct timeval start,end;
+    gettimeofday(&start, NULL);
+    pthread_t pushThread, popThread;
+    pthread_create(&pushThread, NULL, PushThread, &inQueue);
+    pthread_create(&popThread, NULL, PopThread, &inQueue);
+
+    pthread_join(pushThread, NULL);
+    pthread_join(popThread, NULL);
+    gettimeofday(&end, NULL);
+
+    cout <<"end! spend time: "<< (end.tv_sec-start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)/1000 << " ms" << endl;
+
+    return 0;
+}
